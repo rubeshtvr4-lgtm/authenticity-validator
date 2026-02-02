64864abcdb3f1d24adaee5f9b717a5fdc164a3c2
rubeshtvr4-lgtm <rubeshtvr4@gmail.com>
2026-02-02 18:31:43 +0530

    Rename User model to UserModel to fix Linux case sensitivity

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // Ensure this path matches your file structure
+const User = require('../models/UserModel'); // Ensure this path matches your file structure
 
 // REGISTER
 router.post('/register', async (req, res) => {
