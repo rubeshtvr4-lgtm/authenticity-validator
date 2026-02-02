a20f67857ff67744e178a2f1e3f83f8341807cee
rubeshtvr4-lgtm <rubeshtvr4@gmail.com>
2026-02-02 18:25:06 +0530

    Add missing User model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email:    { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-=======
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email:    { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
-
->>>>>>> 04e92bd834593c8fa4360410d354b0903e4d7c24
 module.exports = mongoose.model('User', UserSchema);
